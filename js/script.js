57b0e72f13b1249b797b0292856f349f5fce716e
Denys Bludov <denys.bludov@stu.ocsb.ca>
2025-04-01 14:41:56 +0000

    Finished the webpage

--- js/script.js
+++ js/script.js
@@ -3,3 +3,18 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+//Functions that calculate input and give answers
+function calcPerimeter() {
+    let length = document.getElementById("length-peri").value;
+    let width = document.getElementById("width-peri").value;
+    peri = 2*length + 2*width
+    document.getElementById("answer-peri").innerHTML = "Answer:" + peri +" cm";
+}
+
+function calcArea() {
+    let arealength = document.getElementById("length-area").value;
+    let areawidth = document.getElementById("width-area").value;
+    area = arealength*areawidth
+    document.getElementById("answer-area").innerHTML = "Answer:" + area +" cm";
+}
